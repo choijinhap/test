545da506a25bd526cff76f0e9f7f1eaec8a848d4
choijinhap <wlsgkq123@gmail.com>
2022-07-17 13:06:41 +0900

    eslint autoclosing tag

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Counter from "./components/Counter/Counter";
 
 function App() {
-  return <Counter></Counter>;
+  return <Counter />;
 }
 
 export default App;
--- src/components/Counter/Counter.test.js
+++ src/components/Counter/Counter.test.js
@@ -8,25 +8,25 @@ test("the counter starts at 0", () => {
 });
 
 test("minus button has correct text", () => {
-  render(<Counter></Counter>);
+  render(<Counter />);
   const buttonEl = screen.getByTestId("minus-button");
   expect(buttonEl).toHaveTextContent("-");
 });
 test("plus button has correct text", () => {
-  render(<Counter></Counter>);
+  render(<Counter />);
   const buttonEl = screen.getByTestId("plus-button");
   expect(buttonEl).toHaveTextContent("+");
 });
 
 test("when the + button is pressed, the counter changes to 1", () => {
-  render(<Counter></Counter>);
+  render(<Counter />);
   const buttonEl = screen.getByTestId("plus-button");
   fireEvent.click(buttonEl);
   const counterEl = screen.getByTestId("counter");
   expect(counterEl).toHaveTextContent(1);
 });
 test("when the - button is pressed, the counter changes to 1", () => {
-  render(<Counter></Counter>);
+  render(<Counter />);
   const buttonEl = screen.getByTestId("minus-button");
   fireEvent.click(buttonEl);
   const counterEl = screen.getByTestId("counter");
@@ -34,12 +34,12 @@ test("when the - button is pressed, the counter changes to 1", () => {
 });
 
 test("on/off button color is blue", () => {
-  render(<Counter></Counter>);
+  render(<Counter />);
   const buttonEl = screen.getByTestId("onoff-button");
   expect(buttonEl).toHaveStyle({ backgroundColor: "blue" });
 });
 test("Prevent the +/- button from being pressed when the on/off button is clicked", () => {
-  render(<Counter></Counter>);
+  render(<Counter />);
   const onoffButtonEl = screen.getByTestId("onoff-button");
   fireEvent.click(onoffButtonEl);
   const plusButtonEl = screen.getByTestId("plus-button");
