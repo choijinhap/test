fe4b5638b266d590773e66bc53b977aeb6f4f068
choijinhap <wlsgkq123@gmail.com>
2022-07-17 16:39:12 +0900

    UI 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import Counter from "./components/Counter/Counter";
+import OrderPage from "./pages/OrderPage/OrderPage";
 import Type from "./pages/OrderPage/Type";
 import SummaryPage from "./pages/SummaryPage/SummaryPage";
 
 function App() {
-  return <Type orderType="products" />;
+  return (
+    <div style={{ padding: "4rem" }}>
+      <OrderPage />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/OrderPage/Type.jsx
+++ src/pages/OrderPage/Type.jsx
@@ -32,7 +32,22 @@ const Type = (props) => {
         imagePath={item.imagePath}
       />
     ));
-  return <div>{optionItems}</div>;
+  return (
+    <>
+      <h2>주문 종류</h2>
+      <p>하나의 가격</p>
+      <p>총 가격</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: orderType === "options" && "column",
+        }}
+      >
+        {" "}
+        {optionItems}
+      </div>
+    </>
+  );
 };
 
 export default Type;
