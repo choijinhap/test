f4a88ffa0b707fcff997edd3f68a6c1c47285c55
choijinhap <wlsgkq123@gmail.com>
2022-07-17 15:21:55 +0900

    msw 서비스워커 설정

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { serviceWorker } from "./mocks/browser";
+serviceWorker.start({ onUnhandledRequest: "bypass" });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
